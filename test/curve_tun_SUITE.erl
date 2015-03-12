9e892ff97e85ffebb0a25e00476802d0a0fa53fe
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2015-03-12 19:41:33 +0100

    Add a standard send/recv test to the test SUITE.
    This defines a simple test where we connect two endpoints and verify that we can send 3 messages over the wire. We randomly sleep in the test and repeat it 30 times to make sure that at least some of the possible races are not there.

--- test/curve_tun_SUITE.erl
+++ test/curve_tun_SUITE.erl
@@ -7,7 +7,9 @@
 	 init_per_suite/1, end_per_suite/1,
 	 init_per_testcase/2, end_per_testcase/2]).
 
--export([suite_test/1]).
+-export([send_recv/1]).
+
+-define(TIMEOUT, 5000).
 
 suite() ->
     [{timetrap, {seconds, 10}}].
@@ -23,7 +25,9 @@ end_per_group(_Group, _Config) ->
 init_per_suite(Config) ->
     error_logger:tty(false), %% Disable the error loggers tty output for tests
     {ok, Apps} = application:ensure_all_started(curve_tun),
-    [{apps, Apps} | Config].
+    PK = <<81,13,101,52,29,109,136,196,86,91,34,91,3,19,150,3,215,
+    		43,210,9,242,146,119,188,153,245,78,232,94,113,37,47>>,
+    [{apps, Apps}, {host, "localhost"}, {port, 1337}, {receiver_pk, PK}| Config].
 
 end_per_suite(Config) ->
     Apps = proplists:get_value(apps, Config),
@@ -39,13 +43,66 @@ end_per_testcase(_Case, _Config) ->
 %% Tests
 %% ----------------------------------------------------------------------
 groups() ->
-    [{basic, [shuffle], [
-        suite_test
+    [{basic, [shuffle, {repeat, 30}], [
+        send_recv
     ]}].
 
 all() ->
     [{group, basic}].
 
-suite_test(_Config) ->
-    ct:log("Running test suite for curve_tun"),
+send_recv(Config) ->
+    SPid = sender(Config),
+    RPid = receiver(Config),
+    ok = join([SPid, RPid]),
     ok.
+
+%% -------------------------------------
+join([]) -> ok;
+join([P | Next]) ->
+    receive
+        {P, ok} -> join(Next);
+        {P, {error, Err}} -> ct:fail(Err)
+   after ?TIMEOUT ->
+       ct:fail(timeout)
+   end.
+   
+sender(Config) ->
+    Ctl = self(),
+    spawn(fun() ->
+        random:seed(erlang:now()),
+        ct:sleep(10),
+        sleep(),
+        {ok, Sock} = curve_tun:connect(?config(host, Config), ?config(port, Config),
+            [{key, ?config(receiver_pk, Config)}]),
+        sleep(),
+        ok = curve_tun:send(Sock, <<"1">>),
+        sleep(),
+        ok = curve_tun:send(Sock, <<"2">>),
+        sleep(),
+        ok = curve_tun:send(Sock, <<"3">>),
+        sleep(),
+        ok = curve_tun:close(Sock),
+        Ctl ! {self(), ok}
+    end).
+    
+receiver(Config) ->
+    Ctl = self(),
+    spawn(fun() ->
+        random:seed(erlang:now()),
+        ok = curve_tun_simple_registry:register({127,0,0,1},<<81,13,101,52,29,109,136,196,86,91,34,91,3,19,150,3,215, 43,210,9,242,146,119,188,153,245,78,232,94,113,37,47>>),
+        {ok, LSock} = curve_tun:listen(?config(port, Config), [{reuseaddr, true}]),
+        sleep(),
+        {ok, Sock} = curve_tun:accept(LSock),
+        sleep(),
+        <<"1">> = curve_tun:recv(Sock),
+        sleep(),
+        <<"2">> = curve_tun:recv(Sock),
+        sleep(),
+        <<"3">> = curve_tun:recv(Sock),
+        sleep(),
+        ok = curve_tun:close(Sock),
+        Ctl ! {self(), ok}
+    end).
+
+sleep() ->
+    ct:sleep(random:uniform(100)).
